9b61f523b1ff0537eb01f8e31f83ded27a11d553
shota8888 <asbscs@icloud.com>
2021-03-29 14:42:26 +0900

    refactor:ESLint導入に伴うコードのリファクタ

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+const App = (): JSX.Element => {
+  return <div></div>
 }
 
-export default App;
+export default App
