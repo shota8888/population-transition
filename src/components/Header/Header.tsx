862440febbbb6139b7e72f8443518784e328034c
shota8888 <asbscs@icloud.com>
2021-03-29 17:14:40 +0900

    style:Headerのstyleを記述

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 15%);
+`
+
+const Title = styled.h1`
+  font-size: 1.5em;
+`
 
 export const Header = (): JSX.Element => {
   return (
     <div>
-      <h1>都道府県別の総人口推移グラフ</h1>
+      <Container>
+        <Title>都道府県別の総人口推移グラフ</Title>
+      </Container>
     </div>
   )
 }
