4b794b111a6fd105e97cc19c1279bff9be359552
shota8888 <asbscs@icloud.com>
2021-03-29 16:53:16 +0900

    style:リセット用のstyleを記述

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { GlobalStyle } from './styles/global'
 
 const App = (): JSX.Element => {
-  return <div></div>
+  return (
+    <>
+      <GlobalStyle />
+      <div></div>
+    </>
+  )
 }
 
 export default App
