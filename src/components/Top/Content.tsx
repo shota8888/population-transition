27bb0ecee2b0779bbc0ae1f83599ab53d5e68751
shota8888 <asbscs@icloud.com>
2021-03-30 15:35:58 +0900

    feat:都道府県をcheckしたときに人口データをグラフへ反映させるように実装

--- src/components/Top/Content.tsx
+++ src/components/Top/Content.tsx
@@ -1,5 +1,7 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { useFetch } from '../../hooks/useFetch'
+import { Population } from '../../types/Population'
 import { Prefectures } from '../../types/Prefectures'
 import { PopChartData, PopulationChart } from '../common/Chart/PopulationChart'
 import PrefecturesList from './PrefecturesList'
@@ -27,14 +29,56 @@ type Props = {
 }
 
 export const Content = ({ items }: Props): JSX.Element => {
-  const [selected, setSelected] = useState<boolean[]>(Array(47).fill(false))
+  const initialData: Population = {
+    message: null,
+    result: {
+      boundaryYear: 0,
+      data: [],
+    },
+  }
+  const url = '/api/v1/population/composition/perYear?cityCode=-&prefCode=1'
+
+  const { state, fetchData } = useFetch<Population>(initialData, url)
+
+  const [selected, setSelected] = useState<boolean[]>(() =>
+    items.map(() => false)
+  )
+  const [targetPref, setTargetPref] = useState('')
+  const [populationData, setPopulationData] = useState<PopChartData>([])
 
   const handleChangeSelection = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
       setSelected((checks) => checks.map((c, i) => (i === index ? !c : c)))
+      setTargetPref(e.target.name)
+
+      if (e.target.checked) {
+        const url = `/api/v1/population/composition/perYear?cityCode=-&prefCode=${e.target.id}`
+        fetchData(url)
+      } else {
+        populationData.map((d) => delete d[e.target.name])
+      }
     },
-    []
+    [fetchData, populationData]
   )
+
+  useEffect(() => {
+    if (state.data.result.data.length) {
+      if (!populationData.length) {
+        const tmp: PopChartData = state.data.result.data[0].data.map((d) => ({
+          year: d.year,
+        }))
+        setPopulationData(tmp)
+      } else {
+        const arr = state.data.result.data[0].data.map((d) => ({
+          [targetPref]: d.value,
+        }))
+        const popData = populationData.map((d, i) =>
+          Object.assign({}, d, arr[i])
+        )
+        setPopulationData(popData)
+      }
+    }
+  }, [state])
 
   return (
     <div>
@@ -46,7 +90,7 @@ export const Content = ({ items }: Props): JSX.Element => {
         />
       </ListContainer>
       <ChartContainer>
-        <PopulationChart data={data} />
+        <PopulationChart data={populationData} />
       </ChartContainer>
     </div>
   )
