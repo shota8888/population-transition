6126ec57873de4b0e2b1f59b050fa6af33f8d869
shota8888 <asbscs@icloud.com>
2021-03-30 11:53:33 +0900

    style:CheckBoxのstyleを記述

--- src/components/common/Forms/CheckBox.tsx
+++ src/components/common/Forms/CheckBox.tsx
@@ -1,4 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Label = styled.label`
+  display: block;
+  position: relative;
+  padding-left: 30px;
+  margin-bottom: 15px;
+  cursor: pointer;
+`
+
+const Input = styled.input`
+  height: 0;
+  width: 0;
+  opacity: 0;
+  z-index: -1;
+`
+
+const Indicator = styled.div`
+  position: absolute;
+  top: -2px;
+  left: 0;
+  height: 20px;
+  width: 20px;
+  border: 1px solid ${(props) => props.theme.palette.border.main};
+
+  &::after {
+    content: '';
+    position: absolute;
+    display: none;
+  }
+
+  ${Input}:checked + &::after {
+    display: block;
+    left: 8px;
+    top: 4px;
+    width: 3px;
+    height: 8px;
+    border: solid ${(props) => props.theme.palette.primary};
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
+  }
+`
 
 type Props = {
   item: { id: string; name: string }
@@ -9,8 +51,8 @@ type Props = {
 export const CheckBox = ({ item, checked, onChange }: Props): JSX.Element => {
   return (
     <div>
-      <label html-for={item.id.toString()}>
-        <input
+      <Label html-for={item.id.toString()}>
+        <Input
           type="checkbox"
           id={item.id.toString()}
           name={item.name}
@@ -18,7 +60,8 @@ export const CheckBox = ({ item, checked, onChange }: Props): JSX.Element => {
           onChange={onChange}
         />
         {item.name}
-      </label>
+        <Indicator />
+      </Label>
     </div>
   )
 }
